c0e0e10af1d542ea6a9c2a5341940ae945def745
Tejomurthy G P <tejomurthygp@gmail.com>
2021-04-21 10:52:18 +0530

    first code change by Teju

--- Facebook.java
+++ Facebook.java
@@ -10,10 +10,13 @@ public class Facebook {
 	
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Work\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://www.facebook.com");
 		driver.findElement(By.id("email")).sendKeys("avinashvk05@gmail.com");
 		driver.findElement(By.id("pass")).sendKeys("11111111");
 		driver.findElement(By.name("login")).click();
+		driver.close();
+
 
 	}
 
